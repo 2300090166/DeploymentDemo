0794549a317f1387ececb92d3c8c308faa35bd77
2300090166@kluniversity.in <2300090166@kluniversity.in>
2025-04-09 12:10:21 +0530

    Initial commit

---
+++ src/main/java/com/agribzar/Deployment90166/Deployment90166Application.java
@@ -0,0 +1,13 @@
+package com.agribzar.Deployment90166;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deployment90166Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deployment90166Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/agribzar/Deployment90166/Deployment90166ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agribzar.Deployment90166;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deployment90166ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
